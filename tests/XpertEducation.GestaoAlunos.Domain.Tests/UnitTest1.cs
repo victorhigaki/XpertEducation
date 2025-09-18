596d5e08c48d5617813ce37835349679696ca04c
Victor Higaki <victorhigaki@outlook.com>
2025-09-18 00:31:47 -0300

    feat: tests projects

---
+++ tests/XpertEducation.GestaoAlunos.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace XpertEducation.GestaoAlunos.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/XpertEducation.GestaoConteudo.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace XpertEducation.GestaoConteudo.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
