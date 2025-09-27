900b707afa754dea1620515ac07400ca99c1ed58
Victor Higaki <victorhigaki@outlook.com>
2025-09-27 18:33:18 -0300

    feat: add swagger

--- src/XpertEducation.WebApps.Api/Program.cs
+++ src/XpertEducation.WebApps.Api/Program.cs
@@ -5,17 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddIdentityConfiguration(builder.Configuration);
+
+builder.AddSwaggerConfig();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
