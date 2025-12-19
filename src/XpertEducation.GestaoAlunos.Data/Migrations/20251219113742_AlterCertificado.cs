124c3a12467c709d59292212f0aea84d5d215adc
Victor Higaki <victorhigaki@outlook.com>
2025-12-19 08:38:02 -0300

    feat: Certificado Migration

---
+++ src/XpertEducation.GestaoAlunos.Data/Migrations/20251219113742_AlterCertificado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XpertEducation.GestaoAlunos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterCertificado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AlunoId",
+                table: "Certificado",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CursoId",
+                table: "Certificado",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataEmissao",
+                table: "Certificado",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CursoId",
+                table: "Certificado");
+
+            migrationBuilder.DropColumn(
+                name: "DataEmissao",
+                table: "Certificado");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AlunoId",
+                table: "Certificado",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+        }
+    }
+}
